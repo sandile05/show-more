f178423a249c864bbcb42d0f9f7e037b55356b1e
sandile05 <106535890+sandile05@users.noreply.github.com>
2023-03-26 10:36:59 +0200

    final commit

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ const readMoreBtn = document.querySelector('.read-more-btn');
 const text = document.querySelector('.text');
 
 readMoreBtn.addEventListener('click', (e)=>{
-    text.classList.toggle('show-mow');
+    text.classList.toggle('show-more');
+    if(readMoreBtn.innerText === 'Read More'){
+        readMoreBtn.innerText = 'Read Less';
+    } else{
+        readMoreBtn.innerText = 'Read More';
+    }
 })
